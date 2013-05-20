b54dad9497a5dc440582648fcff17584e22060b1
Wolfram Hempel <hempel.wolfram@gmail.com>
2013-05-20 07:55:58 +0100

    Template html is not escaped anymore

--- build/src/templateHelper.js
+++ build/src/templateHelper.js
@@ -1,7 +1,9 @@
 var fs = require( "fs" );
 var path = require( "path" );
+var hbs = require( 'hbs' );
 
 exports.template = function( sPath )
 {
-	return fs.readFileSync( path.join( CONF.templateFolder, sPath ), "utf8" );
+	var sTemplate = fs.readFileSync( path.join( CONF.templateFolder, sPath ), "utf8" );
+	return new hbs.handlebars.SafeString( sTemplate );
 };
