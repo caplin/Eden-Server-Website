cc15f7d5607a4db27eafa89babc219b0eff611be
Wolfram Hempel <hempel.wolfram@gmail.com>
2013-05-20 17:25:34 +0100

    Added index creation

--- build/src/templateHelper.js
+++ build/src/templateHelper.js
@@ -2,8 +2,19 @@ var fs = require( "fs" );
 var path = require( "path" );
 var hbs = require( 'hbs' );
 
-exports.template = function( sPath )
+exports.template = function( sPath, vData )
 {
 	var sTemplate = fs.readFileSync( path.join( CONF.templateFolder, sPath ), "utf8" );
+
+	if( vData )
+	{
+		sTemplate = hbs.handlebars.compile( sTemplate )( vData );
+	}
+
 	return new hbs.handlebars.SafeString( sTemplate );
+};
+
+exports.toURL = function( sPath )
+{
+	return encodeURIComponent( sPath );
 };
